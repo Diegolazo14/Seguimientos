901fd9b6b69edc8ec3fc582bd7606957f7cfd78a
Diegolazo14 <diego.astorgamunoz@gmail.com>
2025-01-17 18:35:11 -0300

    Update FirebaseConfig.js

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -2,17 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxbCqk4X-ALAMptP6YDOFA1A-OWhPDdSg",
-  authDomain: "seguimientopartido.firebaseapp.com",
-  projectId: "seguimientopartido",
-  storageBucket: "seguimientopartido.firebasestorage.app",
-  messagingSenderId: "58770097357",
-  appId: "1:58770097357:web:cff584fbb412660da71584",
-  measurementId: "G-BWFP38F2BH"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
-
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app, "https://seguimientopartido-default-rtdb.firebaseio.com");
+const database = getDatabase(app);
 
 export default database;
