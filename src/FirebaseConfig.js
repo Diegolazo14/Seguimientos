15b630be9e160fbd5e3c8271e8a6a8198620d110
Diegolazo14 <diego.astorgamunoz@gmail.com>
2025-01-17 10:40:34 -0300

    Update FirebaseConfig.js

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -13,6 +13,6 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const database = getDatabase(app, "https://seguimientopartido-default-rtdb.firebaseio.com");
 
 export default database;
